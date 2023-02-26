5d3c1e6f04ee9d9dd2e60963530c57979729e354
Oyetade Segun <johnwellacademy@gmail.com>
2023-02-26 06:27:57 -0800

    ff

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <string.h>
 /**
  * main - A C program that prints a line to the standard error
  * Return 1 (Success)
  */
 int main(void)
 {
-fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return (1);
+	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(STDERR_FILENO, message, strlen(message));
+	return (1);
 }
