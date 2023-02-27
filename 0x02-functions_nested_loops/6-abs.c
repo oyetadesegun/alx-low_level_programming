ef0dd05637d050c2ef502ae7f9038c94d0604e72
Oyetade Segun <johnwellacademy@gmail.com>
2023-02-27 06:48:19 -0800

    nexted loop

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * main - to fid the absolute value of an integer
- * Return 0 (Success)
+ * _abs - to fid the absolute value of an integer
+ * @a: the int in question
+ * Return: abs (Success)
  */
 int _abs(int a)
 {
-	if (a < 0)
-		a= -(a);
-	else if (a >= 0)
-		a= a;
-	return (a);
+	if (a >= 0)
+		return (a);
+	else
+		return (-a);
 }
