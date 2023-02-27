6abd0776a31be17a06c40b9e4114afdcd4da1eca
Oyetade Segun <johnwellacademy@gmail.com>
2023-02-27 07:17:08 -0800

    nexted loop

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * jack_bauer - Print every minute of the day of jack Bauer 
+ *jack_bauer - Print every minute of the day of jack Bauer
  *
  */
 void jack_bauer(void)
@@ -9,7 +9,7 @@ void jack_bauer(void)
 
 	for (hour = 0; hour <= 23; hour++)
 	{
-		for (minute =0; minute <= 59; minute++)
+		for (minute = 0; minute <= 59; minute++)
 		{
 			_putchar((hour / 10) + '0');
 			_putchar((hour % 10) + '0');
