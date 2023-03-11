611693ca23ac0feb479482e911b9948516c83e19
oyetadesegun <johnwellacademy@gmail.com>
2023-03-11 01:41:17 +0100

    nexted loop

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - to fid the absolute value of an integer
+ * @a: the int in question
+ * Return: abs (Success)
+ */
+int _abs(int a)
+{
+	if (a >= 0)
+		return (a);
+	else
+		return (-a);
+}
