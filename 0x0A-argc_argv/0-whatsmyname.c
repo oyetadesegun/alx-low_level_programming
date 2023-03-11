37a0f49feee6f0a049e1d69bce8a2ca863f2aff2
oyetadesegun <johnwellacademy@gmail.com>
2023-03-11 03:36:02 +0100

    nexted loop

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * min - a function that prints name by new file 
+ * main - a function that prints name by new file 
  * @argc: argc parameter
  * @argv: an array of a command listed
  * Return: 0 (success)
