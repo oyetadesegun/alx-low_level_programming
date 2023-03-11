505eb9bc512c395422c87e3590fb1b1da883bcd4
oyetadesegun <johnwellacademy@gmail.com>
2023-03-11 03:38:37 +0100

    nexted loop

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,8 +5,8 @@
  * @argv: an array of a command listed
  * Return: 0 (success)
 */
-int main (int argc, char *argv[])
+int main (int argc, char **argv)
 {
-    printf("%s\n", *argv);
+    printf("%s\n", argv[0]);
     return (0);
 }
