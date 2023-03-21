3179c406ee43f0a973cb37c769425290b547e1b1
oyetadesegun <johnwellacademy@gmail.com>
2023-03-21 14:21:43 +0100

    Newly added

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
- * main - print the name of the file
- * Return: always 0
+ *main - print the name of the file
+ * 
+ *Return: always 0
  */
 
 int main(void)
 {
-	printf("%s\n",__FILE__);
+	printf("%s\n", __FILE__);
 	return (0);
 }
