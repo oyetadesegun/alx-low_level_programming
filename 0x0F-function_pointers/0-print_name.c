44eec54cb8f8728c49a426c28497b3e82dc5a352
oyetadesegun <johnwellacademy@gmail.com>
2023-03-23 00:01:55 +0100

    Newly added

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ *print_name - Funcion that prints a name
+ *@name: name of the person
+ *@f: pointer to function
+ * Return: Nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if(name == NULL || f == NULL)
+		return;
+	f(name);
+}
